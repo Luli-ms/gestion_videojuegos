2ce3bd5e0978a8a741516bfc4c560f53ff35bdd1
Luli <lulifmolina@gmail.com>
2026-02-03 14:41:36 +0100

    Initial Commit. Creación del proyecto

---
+++ src/test/java/com/utad/gestion_videojuegos/GestionVideojuegosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utad.gestion_videojuegos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVideojuegosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
